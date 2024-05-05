647e54cca1eca9cc5e34b7a8c520c8f5c94951ac
Ayush Mishra (techy boy) <ayushmishra9799@gmail.com>
2024-05-05 11:25:50 +0530

    djd

---
+++ qtify/src/components/FeedbackButton/FeedbackButton.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./FeedbackButton.css";
+
+export default function FeedbackButton(props) {
+  return (
+    <div className="feedback">
+      <h3>Give Feedback</h3>
+    </div>
+  );
+}
+
+// const Feedback = ({ onFeedbackButtonClick }) => {
+//   return (
+//     <div className='feedback'>
+//       <h3 onClick={onFeedbackButtonClick}>Give Feedback</h3>
+//     </div>
+//   );
+// }
+
+// export default Feedback
